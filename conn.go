2c5e18914333a0f0855530403a44f0d5845898c0
heshan <5373827@qq.com>
2021-10-23 11:24:17 +0800

    base framework

--- conn.go
+++ conn.go
@@ -1,6 +1,7 @@
 package hio
 
 import (
+	"errors"
 	"fmt"
 	"syscall"
 )
@@ -20,13 +21,23 @@ type Conn struct {
 	in    []byte
 	out   []byte
 	state ConnState
+	attrs map[string]interface{}
 }
 
-func (t *Conn) Write(data []byte) {
+func (t *Conn) Write(data []byte) error {
+	if t.state == ConnState_Close {
+		return errors.New("can't write to a closed connection")
+	}
+
 	t.out = append(t.out, data...)
+	return nil
 }
 
 func (t *Conn) Close() {
+	if t.state == ConnState_Close {
+		return
+	}
+
 	t.state = ConnState_Close
 	t.loop.tryCloseConn(t)
 }
@@ -35,14 +46,40 @@ func (t *Conn) String() string {
 	return fmt.Sprintf("%v", t.id)
 }
 
+func (t *Conn) Id() int {
+	return t.id
+}
+
+func (t *Conn) State() ConnState {
+	return t.state
+}
+
+func (t *Conn) Attr(key string) (interface{}, bool) {
+	v, b := t.attrs[key]
+	return v, b
+}
+
+func (t *Conn) SetAttr(key string, value interface{}) {
+	if value == nil {
+		delete(t.attrs, key)
+	} else {
+		t.attrs[key] = value
+	}
+}
+
+func (t *Conn) EventLoop() *EventLoop {
+	return t.loop
+}
+
 func newConn(loop *EventLoop, id int, fd int, addr syscall.Sockaddr) *Conn {
 	conn := &Conn{
-		id:   id,
-		loop: loop,
-		fd:   fd,
-		addr: addr,
-		in:   make([]byte, 4096, 4096),
-		out:  make([]byte, 0, 4096),
+		id:    id,
+		loop:  loop,
+		fd:    fd,
+		addr:  addr,
+		in:    make([]byte, 4096, 4096),
+		out:   make([]byte, 0, 4096),
+		attrs: make(map[string]interface{}),
 	}
 
 	return conn
