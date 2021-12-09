fa21bd442b92717d03b0e2deddca00575a5be1fd
heshan <5373827@qq.com>
2021-12-09 11:03:48 +0800

    add echo example

---
+++ example/echo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/scheshan/hio"
+	"github.com/scheshan/hio/buf"
+	"log"
+	"time"
+)
+
+func main() {
+	opt := hio.ServerOptions{}
+	opt.EventLoopNum = 4
+	opt.OnSessionCreated = func(conn *hio.Conn) {
+		log.Printf("connection[%v] created, bind to EventLoop[%v]", conn, conn.EventLoop().Id())
+	}
+	opt.OnSessionRead = func(conn *hio.Conn, buffer *buf.Buffer) {
+		log.Printf("connection[%v] receive data: %v", conn, buffer.ReadableBytes())
+
+		conn.Write(buffer)
+	}
+	opt.OnSessionClosed = func(conn *hio.Conn) {
+		log.Printf("connection[%v] closed", conn)
+	}
+
+	srv, err := hio.Serve("16379", opt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-time.After(1 * time.Hour)
+
+	srv.Shutdown()
+}
